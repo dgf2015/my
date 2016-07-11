21a1dfd259d08b5c069f9d0466843f0408d20a84
yootk <mldnqa@163.com>
2016-07-11 11:39:07 +0800

    chang hello.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@
 
    public static void main(String args[]){
     System.out.println("Hello World !");
- System.out.println("Hello World !!!");
+ System.out.println("Hello World !!!"+"diff");
  System.out.println("Hello World !!!");
 }
 
