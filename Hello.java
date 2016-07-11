c675d031844c0f0c75870bfc772948c91cb0403b
yootk <mldnqa@163.com>
2016-07-11 16:35:29 +0800

    hello.java 更新

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@
     System.out.println("Hello World !");
  System.out.println("Hello World !!!"+"diff");
  System.out.println("Hello World !!!");
- System.out.println("Hello World !!!");
+ System.out.println("更新成功！");
 }
 
 }
